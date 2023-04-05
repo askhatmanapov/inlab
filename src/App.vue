e9d77b02bb4a6e24b761d24a4366479d349c445f
Askhat Manapov <askhatmanapov.02@gmail.com>
2023-04-06 00:18:39 +0600

    modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -39,10 +39,10 @@
 
 
 
-  import * as pdfjsLib from 'pdfjs-dist';
-  const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.entry');
+  // import * as pdfjsLib from 'pdfjs-dist';
+  // const pdfjsWorker = await import('pdfjs-dist/build/pdf.worker.entry');
 
-  // import pdfjsWorker from './pdfjs-dist/build/pdf.worker.entry';
+  import pdfjsWorker from './pdfjs-dist/build/pdf.worker.entry';
 
 
 
