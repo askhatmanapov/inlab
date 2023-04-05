a1f480dd4316cf75db26679152b2f35d18dba721
Askhat Manapov <askhatmanapov.02@gmail.com>
2023-04-05 17:11:11 +0600

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,13 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// vite.config.js
+
+module.exports = {
+  target: 'es2019'
+  // ... other config options
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
